0e8402d0e65ab9447fb4e36a94a545a9b1ea7aa9
Sergei Rodionov <seserodionov@edu.hse.ru>
2026-04-02 00:38:02 +0500

    Сделал небольшой тестик

--- ViewModels/MainViewModels.cs
+++ ViewModels/MainViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Graphics.Canvas.Text;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 
@@ -8,18 +9,53 @@ namespace NooBasket.ViewModels
     public partial class MainViewModels : ObservableObject
     {
         [ObservableProperty]
-        private int passwordLenght = 8;
+        private Tests test = new Tests("Программист ставит на тумбочку перед сном два стакана. " +
+            "Один с водой — на случай, если захочет пить. А зачем второй, пустой?", 
+            ["Для зубных протезов", "Для симметрии", "На случай, если НЕ захочет пить", "Чтобы жена не ругалась"], 2);
 
         [ObservableProperty]
-        private string result = "Пароль появится здесь";
+        private Color newColor1 = Colors.Blue;
+
+        [ObservableProperty]
+        private Color newColor2 = Colors.Blue;
+
+        [ObservableProperty]
+        private Color newColor3 = Colors.Blue;
+
+        [ObservableProperty]
+        private Color newColor4 = Colors.Blue;
 
         [RelayCommand]
-        private void GeneratePass()
+        private void ReplayColor1()
         {
-            string chars = "QwErTyUiOpAsDfGhJkLzXcVbNm1234567890";
-            Random rand = new Random();
-            Result = new string(Enumerable.Repeat(chars, PasswordLenght)
-                .Select(s => s[rand.Next(s.Length)]).ToArray());
+            NewColor1 = NewColor2 = NewColor3 = NewColor4 = Colors.Blue;
+            if (test.CorrectAnswer == 0)
+                NewColor1 = Colors.Green;
+            else NewColor1 = Colors.Red;
+        }
+        [RelayCommand]
+        private void ReplayColor2()
+        {
+            NewColor1 = NewColor2 = NewColor3 = NewColor4 = Colors.Blue;
+            if (test.CorrectAnswer == 1)
+                NewColor2 = Colors.Green;
+            else NewColor2 = Colors.Red;
+        }
+        [RelayCommand]
+        private void ReplayColor3()
+        {
+            NewColor1 = NewColor2 = NewColor3 = NewColor4 = Colors.Blue;
+            if (test.CorrectAnswer == 2)
+                NewColor3 = Colors.Green;
+            else NewColor3 = Colors.Red;
+        }
+        [RelayCommand]
+        private void ReplayColor4()
+        {
+            NewColor1 = NewColor2 = NewColor3 = NewColor4 = Colors.Blue;
+            if (test.CorrectAnswer == 3)
+                NewColor4 = Colors.Green;
+            else NewColor4 = Colors.Red;
         }
     }
 }
